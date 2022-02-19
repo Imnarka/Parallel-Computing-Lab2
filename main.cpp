29098b04076dc904ace39fad1e4aeefe73111583
lmnarka <lnarka09@gmail.com>
2022-02-19 10:27:09 +0500

    Described functions

--- main.cpp
+++ main.cpp
@@ -9,58 +9,50 @@
 
 using std::cout;
 
-int main()
-{
-	int N, rank, k, i; // num of threads, Array size, rank
-	float Sum = 0;
-	scanf("%d", &k);
-	scanf("%d", &N);
-	omp_set_num_threads(k);
+void problem1(int rank) {
 #pragma omp parallel shared(rank)
 	{
 		rank = omp_get_thread_num();
 		Sleep(100);
 		printf("I am %d thread\n", rank);
 	}
-	cout << "*******************\n";
+}
+
+void problem2(int rank) {
 #pragma omp parallel private(rank)
 	{
 		rank = omp_get_thread_num();
 		printf("I am %d thread\n", rank);
 	}
+}
 
-	float* Array = new float[N];
-	srand(time(NULL));
-	for (int i = 0; i < N; i++) {
-		
-		Array[i] = (double)rand() / RAND_MAX * (23*3 - (23-3)) + (23-3);
-	}
-
+void problem3(int N, float* Array, float Sum) {
 #pragma omp parallel num_threads(2) reduction(+:Sum)
 	{
-		if (omp_get_thread_num() != 1){
-			for (int i = 0; i < N / 2; i++){
+		if (omp_get_thread_num() != 1) {
+			for (int i = 0; i < N / 2; i++) {
 				Sum += Array[i];
 			}
 		}
-		else{
-			for (int i = N/2; i < N; i++){
+		else {
+			for (int i = N / 2; i < N; i++) {
 				Sum += Array[i];
 			}
 		}
 		printf("[%d]: Sum = %f\n", omp_get_thread_num(), Sum);
 	}
 	printf("Sum = %f\n", Sum);
-	cout << "*************************\n";
-	k = 0;
-	scanf("%d", &k);
+}
+
+void problem4(int N, float* Array, float Sum, int k) {
 	
+	scanf("%d", &k);
 	omp_set_num_threads(k);
-	Sum = 0;
+
 #pragma omp parallel num_threads(k) reduction(+:Sum)
 	{
-		int lowerBound = omp_get_thread_num() * N / k;
-		int upperBound = (omp_get_thread_num() + 1) * N / k;
+		int lowerBound = (N / k) * omp_get_thread_num();
+		int upperBound = (N / k) * (omp_get_thread_num() + 1);
 
 		for (int i = lowerBound; i < upperBound; i++) {
 			Sum += Array[i];
@@ -68,6 +60,29 @@ int main()
 		printf("[%d]: Sum = %f\n", omp_get_thread_num(), Sum);
 	}
 	printf("Sum = %f\n", Sum);
+}
 
+int main()
+{
+	int N, rank, k, i; // num of threads, Array size, rank
+	float Sum = 0;
+	scanf("%d", &k);
+	scanf("%d", &N);
+	omp_set_num_threads(k);
+	problem1(rank);
+	cout << "*******************\n";
+	problem2(rank);
+
+	float* Array = new float[N];
+	srand(time(NULL));
+	for (int i = 0; i < N; i++) {
+		
+		Array[i] = (double)rand() / RAND_MAX * (23*3 - (23-3)) + (23-3);
+	}
+	problem3(N, Array, Sum);
+	cout << "*******************\n";
+	Sum = 0;
+	k = 0;
+	problem4(N, Array, Sum,k);
 	return 0;
 }
